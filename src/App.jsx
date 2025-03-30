425561cb4354cb456c99f051cf596bf15f5f0ad3
maluv3106 <maluv3106@gmail.com>
2025-03-30 18:44:50 +0000

    criação da função para atualizar o card

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import iconStar from "./images/icon-star.svg"
 
 export function App(){
   const [notaAvaliacao, setNotaAvaliacao] = useState(0)
+  const [submited, setSubmited] = useState(false)
 
   function handleAlterarNotaAvaliacao(nota){
     setNotaAvaliacao(nota)
@@ -10,26 +11,40 @@ export function App(){
     console.log(nota)
   }
 
-  return(
+  function handleSubmit(){
+    setSubmited(true)
+
+  }
+
+  return (
     <div className="bg-gradient-dark mx-6 p-6 rounded-2xl text-white font-overpass">
-      <div className="bg-dark-blue p-4
-       w-fit rounded-full mb-4">
-        <img src={ iconStar } alt="" />
-      </div>
-
-      <h1 className="text-2xl font-bold mb-2.5">How did we do?</h1>
-
-      <p className="text-light-grey text-sm leading-1 mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-
-      <div className="flex justify-between mb-6">
-        <input type="button" value={1} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey" onClick={() => handleAlterarNotaAvaliacao(1)}/>
-        <input type="button" value={2} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey" onClick={() => handleAlterarNotaAvaliacao(2)}/>
-        <input type="button" value={3} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey" onClick={() => handleAlterarNotaAvaliacao(3)}/>
-        <input type="button" value={4} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey" onClick={() => handleAlterarNotaAvaliacao(4)}/>
-        <input type="button" value={5}className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey" onClick={() => handleAlterarNotaAvaliacao(5)}/>
-      </div>
-
-      <button className="bg-orange w-full py-3 uppercase text-sm rounded-3xl tracking-1 font-bold">Submit</button>
+      {submited === false ? (
+        <>
+          <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
+            <img src={iconStar} alt="" />
+          </div>
+  
+          <h1 className="text-2xl font-bold mb-2.5">How did we do?</h1>
+  
+          <p className="text-light-grey text-sm leading-1 mb-6">
+            Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
+          </p>
+  
+          <div className="flex justify-between mb-6">
+            <input type="button" value={1} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey" onClick={() => handleAlterarNotaAvaliacao(1)} />
+            <input type="button" value={2} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey" onClick={() => handleAlterarNotaAvaliacao(2)} />
+            <input type="button" value={3} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey" onClick={() => handleAlterarNotaAvaliacao(3)} />
+            <input type="button" value={4} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey" onClick={() => handleAlterarNotaAvaliacao(4)} />
+            <input type="button" value={5} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey" onClick={() => handleAlterarNotaAvaliacao(5)} />
+          </div>
+  
+          <button className="bg-orange w-full py-3 uppercase text-sm rounded-3xl tracking-1 font-bold" onClick={handleSubmit}>
+            Submit
+          </button>
+        </>
+      ) : (
+        <p>Outro código</p>
+      )}
     </div>
-  )
+  );
 }
