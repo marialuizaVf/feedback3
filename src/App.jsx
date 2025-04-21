18324a3ec8ae42f593014bdfff63908c6b14e3c6
maluv3106 <maluv3106@gmail.com>
2025-04-21 22:53:02 +0000

    Criação de um componente para o card inicial e tambem para a input button

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,25 @@
-import { useState } from "react"
-import illustrationThankYou from "./images/illustration-thank-you.svg"
-import iconStar from "./images/icon-star.svg"
+import { CardInicial } from "./components/CardInicial"
+ import illustrationThankYou from "./images/illustration-thank-you.svg"
+ import { useState } from "react"
+ 
+ export function App() {
+   const [notaAvaliacao, setNotaAvaliacao] = useState(0)
+   const [submited, setSubmited] = useState(false)
 
-export function App(){
-  const [notaAvaliacao, setNotaAvaliacao] = useState(0)
-  const [submited, setSubmited] = useState(false)
+   return(
+    submited === false ? (
+      <CardInicial setNotaAvaliacao={setNotaAvaliacao} notaAvaliacao={notaAvaliacao} setSubmited={setSubmited}/>
+    ) : (
+      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass max-w-103">
+        <img className="mx-auto mb-6" src={illustrationThankYou} alt="iillustration Thank You" />
 
-  function handleAlterarNotaAvaliacao(nota){
-    setNotaAvaliacao(nota)
+        <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl mb-6">You selected {notaAvaliacao} out of 5</p>
 
-    console.log(nota)
-  }
+        <h1 className="text-2xl font-bold mb-2.5 text-center">Thank You!</h1>
 
-  function handleSubmit(){
-    if (notaAvaliacao !== 0){
-      setSubmited(true)
-      return
-    }
+        <p className="text-sm text-light-grey text-center">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
 
-    alert("Please, choose a rate button!")
-
-  }
-
-  return (
-    <div className="bg-gradient-dark max-w-103 mx-6 p-6 rounded-2xl text-white font-overpass">
-      {submited === false ? (
-        <>
-          <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
-            <img src={iconStar} alt="" />
-          </div>
-  
-          <h1 className="text-2xl font-bold mb-2.5">How did we do?</h1>
-  
-          <p className="text-light-grey text-sm leading-1 mb-6">
-            Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
-          </p>
-  
-          <div className="flex justify-between mb-6">
-            <input type="button" value={1} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus: text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleAlterarNotaAvaliacao(1)} />
-            <input type="button" value={2} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus: text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleAlterarNotaAvaliacao(2)} />
-            <input type="button" value={3} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus: text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleAlterarNotaAvaliacao(3)} />
-            <input type="button" value={4} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus: text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleAlterarNotaAvaliacao(4)} />
-            <input type="button" value={5} className="w-10.5 h-10.5 bg-dark-blue rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus: text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleAlterarNotaAvaliacao(5)} />
-          </div>
-  
-          <button className="bg-orange w-full py-3 uppercase text-sm rounded-3xl tracking-1 font-bold hover:bg-white hover:text-orange cursor-pointer transition duration-400" onClick={handleSubmit}>
-            Submit
-          </button>
-        </>
-      ) : (
-        <div className="bg-gradient-dark mx-6 p-6 rounded-2xl text-white font-overpass text-center">
-         <img className="mx-auto mb-6" src={illustrationThankYou} alt="ilustration Thank You" />
-         
-         <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl">You selected {notaAvaliacao} out of 5!</p>
-
-         <h1 className="text-2xl font-bold mb-2.5">Thank You!</h1>
-  
-          <p className="text-light-grey text-sm leading-1 mb-6">
-          We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!
-          </p>
-  
-         
-         </div>
-      )}
-    </div>
+      </div>
+    )
   )
 }
---
+++ src/components/CardInicial.jsx
@@ -0,0 +1,38 @@
+import iconStar from "../images/icon-star.svg"
+ import { InputButton } from "./InputButton"
+ 
+ export function CardInicial({setNotaAvaliacao, notaAvaliacao, setSubmited}) {
+     
+     function handleSubmite() {
+         if (notaAvaliacao !== 0) {
+           setSubmited(true)
+           return
+         }
+     
+         alert("Please, choose a note!")
+     }
+ 
+     return (
+         <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass max-w-103">
+ 
+             <div className="bg-dark-blue w-fit p-4 rounded-full mb-4">
+             <img src={iconStar} alt=" icon star" />
+             </div>
+ 
+             <h1 className="text-2xl font-bold mb-2.5">How did we do?</h1>
+             <p className="text-sm text-light-grey mb-6 leading-1">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+ 
+             <div className="flex justify-between mb-6">
+                 < InputButton value={1} setNotaAvaliacao={notaAvaliacao} />
+                 < InputButton value={2} setNotaAvaliacao={notaAvaliacao} />
+                 < InputButton value={3} setNotaAvaliacao={notaAvaliacao} />
+                 < InputButton value={4} setNotaAvaliacao={notaAvaliacao} />
+                 < InputButton value={5} setNotaAvaliacao={notaAvaliacao} />
+ 
+             </div>
+ 
+             <button onClick={handleSubmite} className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3 hover:bg-white hover:text-orange cursor-pointer">Submit</button>
+ 
+         </div>
+     )
+ }
